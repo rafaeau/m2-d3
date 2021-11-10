33527cb3b5d83be7edbfc31e52001ea854e317a4
Rafael Ferreira <rafaelnevesf@gmail.com>
2021-11-10 22:48:36 +0100

    Exercise 16

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const travelsCount = function () {
   count.innerHTML = travels.length
 }
 
+const addBadge = function () {
+  let summerCards = document.querySelectorAll("#summer .card-text")
+  for (let i=0; i<summerCards.length; i++){
+    summerCards[i].innerHTML += "<span class='badge badge-danger ml-4'>HOT</span>"
+  }
+  return summerCards
+}
+
 window.onload = function () {
   travelsCount();
+  addBadge();
 }
